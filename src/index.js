39111dfdc511aaad1f1daf5e863d9826f7ec33b0
Guilherme <guilhermejorge272@gmail.com>
2023-07-24 03:56:30 -0300

    Navbar v2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,23 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/style.css';
 
+
+// Dropdown
+const linksDropdown = document.querySelectorAll('.dropdown');
+const dropdownsMenu = document.querySelectorAll('.dropdown-menu');
+
+linksDropdown[0].addEventListener("click", (e) => {
+    e.preventDefault();
+    dropdownsMenu[0].classList.toggle('drop-ativo');
+});
+
+linksDropdown[1].addEventListener("click", (e) => {
+    e.preventDefault();
+    dropdownsMenu[1].classList.toggle('drop-ativo');
+});
+
+linksDropdown[2].addEventListener("click", (e) => {
+    e.preventDefault();
+    dropdownsMenu[2].classList.toggle('drop-ativo');
+});
+
