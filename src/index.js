76e06f07e5db13a49151353b29f5c840cf3bf71b
Guilherme <guilhermejorge272@gmail.com>
2023-07-27 01:05:22 -0300

    Parte 1 do responsivo da navbar pronta

--- src/index.js
+++ src/index.js
@@ -6,20 +6,15 @@ import './assets/css/style.css';
 
 // Dropdown
 const linksDropdown = document.querySelectorAll('.dropdown');
-const dropdownsMenu = document.querySelectorAll('.dropdown-menu');
 
-linksDropdown[0].addEventListener("click", (e) => {
-    e.preventDefault();
-    dropdownsMenu[0].classList.toggle('drop-ativo');
+
+linksDropdown.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const menuDropdown = link.querySelector('.dropdown-menu');
+        menuDropdown.classList.toggle('ativo');
+    });
 });
 
-linksDropdown[1].addEventListener("click", (e) => {
-    e.preventDefault();
-    dropdownsMenu[1].classList.toggle('drop-ativo');
-});
 
-linksDropdown[2].addEventListener("click", (e) => {
-    e.preventDefault();
-    dropdownsMenu[2].classList.toggle('drop-ativo');
-});
 
