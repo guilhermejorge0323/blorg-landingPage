42253d91e6689ac7ce2a497b32694f0be599ae85
Guilherme <guilhermejorge272@gmail.com>
2023-08-11 17:43:42 -0300

    A denovo

--- src/modules/imagesMob.js
+++ src/modules/imagesMob.js
@@ -11,10 +11,10 @@ export function verificaLargura(){
     const image1 = document.querySelector('#img-main1')
     const image3 = document.querySelector('#img-main3')
     if(largura <= 1280){
-        image1.src = '../src/assets/img/illustration-editor-mobile.svg';
-        image3.src = '../src/assets/img/illustration-laptop-mobile.svg';
+        image1.src = 'src/assets/img/illustration-editor-mobile.svg';
+        image3.src = 'src/assets/img/illustration-laptop-mobile.svg';
     }else{
-        image1.src = '../src/assets/img/illustration-editor-desktop.svg';
-        image3.src = '../src/assets/img/illustration-laptop-desktop.svg';
+        image1.src = 'src/assets/img/illustration-editor-desktop.svg';
+        image3.src = 'src/assets/img/illustration-laptop-desktop.svg';
     }
 }
